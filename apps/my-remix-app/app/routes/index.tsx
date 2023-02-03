12a37c58c11f2a4bf8efe98340a964b5501d10bd
Pete Williams <hello@petewritescode.com>
2023-02-03 20:59:08 +0000

    Add shared package to app

--- apps/my-remix-app/app/routes/index.tsx
+++ apps/my-remix-app/app/routes/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from "shared-ui";
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+
+      <Button onClick={() => console.log("clicked!")}>Click me</Button>
+
       <ul>
         <li>
           <a
